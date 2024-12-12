06f65984af599fe2839ffe9f6c6ab4aaad8ca70d
dvir.monajem <dmenajem@gmail.com>
2024-12-12 20:21:18 +0200

    navigation works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatListModule } from "@angular/material/list";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { RouterOutlet } from "@angular/router";
+import { RouterModule, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   imports: [
     RouterOutlet,
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./currency/currency.component").then((m) => m.CurrencyComponent),
+  },
+
+  {
+    path: "history",
+    loadComponent: () =>
+      import("./history/history.component").then((m) => m.HistoryComponent),
+  },
+
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full",
+  },
+];
