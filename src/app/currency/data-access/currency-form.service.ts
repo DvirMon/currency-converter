5e58ee9ef6e8a26ddb6c882ab4cfa6877bcbf860
dvir.monajem <dmenajem@gmail.com>
2024-12-13 12:31:40 +0200

    add form service : 1. form group logic 2. controls validations 3. custom different currency validator

---
+++ src/app/currency/data-access/currency-form.service.ts
@@ -0,0 +1,34 @@
+import { inject } from "@angular/core";
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+
+export function differentCurrenciesValidator(): ValidatorFn {
+  return (formGroup: AbstractControl): ValidationErrors | null => {
+    const from = formGroup.get("from")?.value;
+    const to = formGroup.get("to")?.value;
+
+    return from && to && from === to ? { sameCurrency: true } : null;
+  };
+}
+export class CurrencyFormService {
+  #nfb = inject(NonNullableFormBuilder);
+
+  createCurrencyConverterForm() {
+    return this.#nfb.group(
+      {
+        from: this.#nfb.control("", [Validators.required]),
+        to: this.#nfb.control("", [Validators.required]),
+        amount: this.#nfb.control("", [
+          Validators.required,
+          Validators.pattern(/^[1-9][0-9]*$/),
+        ]),
+      },
+      { validators: [differentCurrenciesValidator()] }
+    );
+  }
+}
