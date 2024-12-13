270a63842eb4becccecb2257292378c68a23512b
dvir.monajem <dmenajem@gmail.com>
2024-12-13 12:40:41 +0200

    add currency interfaces

--- src/app/currency/data-access/currency.model.ts
+++ src/app/currency/data-access/currency.model.ts
@@ -1,3 +1,11 @@
 interface CurrencyItem {
     [code: string]: string; 
-  }
+}
+  
+
+export interface ExchangeRatesResponse {
+  amount: number;
+  base: string;
+  date: string;
+  rates: Record<string, number>; 
+}
