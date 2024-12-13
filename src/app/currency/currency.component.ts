ea8349bb2abc57d25e056748bae4bdedcd85440f
dvir.monajem <dmenajem@gmail.com>
2024-12-13 12:40:21 +0200

    (currency.component) add form logic:
    1. get form values reactivity with obs and signals
    2. use signal to take select values only when changed
    3. use rxjs to take amount value on delay and once for performance
    4. use rxjs + signal to trigger api call only when form is valid and form values has changed

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,9 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { CurrencyService } from "./data-access/currency.service";
-import { JsonPipe } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { CurrencyFormService } from "./data-access/currency-form.service";
+import { CurrencyHttpService } from "./data-access/currency-http.service";
+
+import { toObservable, toSignal } from "@angular/core/rxjs-interop";
+import {
+  combineLatest,
+  debounceTime,
+  filter,
+  map,
+  take
+} from "rxjs";
+import { HistoryService } from "../history/history.service";
 
 @Component({
   selector: "app-currency",
@@ -12,15 +33,71 @@ import { JsonPipe } from "@angular/common";
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
-    JsonPipe
+    MatSelectModule,
+    // JsonPipe,
+    // AsyncPipe,
   ],
   templateUrl: "./currency.component.html",
   styleUrl: "./currency.component.scss",
+  host: { ngSkipHydration: "true" },
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [CurrencyHttpService, CurrencyFormService],
 })
 export class CurrencyComponent {
-  #currencyService = inject(CurrencyService);
+  #currencyHttpService = inject(CurrencyHttpService);
+  #currencyFormService = inject(CurrencyFormService);
+  #historyService = inject(HistoryService);
 
+  currencyResource = this.#currencyHttpService.getCurrencyList();
 
-  currencyResource = this.#currencyService.getCurrencyList();
+  currencyConverterForm: FormGroup<{
+    from: FormControl<string>;
+    to: FormControl<string>;
+    amount: FormControl<string>;
+  }> = this.#currencyFormService.createCurrencyConverterForm();
+
+  toValue = toSignal(this.currencyConverterForm.controls["to"].valueChanges, {
+    initialValue: "",
+  });
+
+  fromValue = toSignal(
+    this.currencyConverterForm.controls["from"].valueChanges,
+    {
+      initialValue: "",
+    }
+  );
+
+  amountValue = toSignal(
+    this.currencyConverterForm.controls["amount"].valueChanges.pipe(
+      debounceTime(300),
+      filter(() => this.currencyConverterForm.controls["amount"].valid),
+      take(1)
+    ),
+    { initialValue: "" }
+  );
+
+  formValue = computed(() => {
+    return {
+      amount: this.amountValue(),
+      from: this.fromValue(),
+      to: this.toValue(),
+    };
+  });
+
+  isFormValid$ = this.currencyConverterForm.statusChanges.pipe(
+    map((status) => status === "VALID")
+  );
+
+  formValues$ = toObservable(this.formValue);
+
+  convertTrigger$ = combineLatest([this.isFormValid$, this.formValues$]).pipe(
+    map(([valid, values]) => (valid ? values : undefined))
+  );
+
+  convertTrigger = toSignal(this.convertTrigger$);
+
+  ratesResource = this.#currencyHttpService.getCurrencyRates(
+    this.convertTrigger
+  );
+
 }
