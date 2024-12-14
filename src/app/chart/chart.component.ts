77c4426ba59ce39ec541d3ac89e0ed833ac6ae43
dvir.monajem <dmenajem@gmail.com>
2024-12-14 11:35:44 +0200

    add chart component

---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChartComponent {
+
+}
