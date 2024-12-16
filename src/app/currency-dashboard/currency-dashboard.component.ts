edf28606a8a72dd35d2a97f6b2d1259b85da54fb
dvir.monajem <dmenajem@gmail.com>
2024-12-16 21:12:06 +0200

    working on dashboard

--- src/app/currency-dashboard/currency-dashboard.component.ts
+++ src/app/currency-dashboard/currency-dashboard.component.ts
@@ -1,12 +1,23 @@
-import { Component, inject } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { Breakpoints, BreakpointObserver } from "@angular/cdk/layout";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { AsyncPipe } from "@angular/common";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { CurrencyHttpService } from "../currency/data-access/currency-http.service";
+import { CurrencyStore } from "../currency/data-access/currency.store";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { CurrencyFormComponent } from "../currency/ui/currency-form/currency-form.component";
+import { CurrencyLineChartComponent } from "../currency/ui/currency-line-chart/currency-line-chart.component";
+import { CurrencyResultComponent } from "../currency/ui/currency-result/currency-result.component";
+import { CurrencyFormService } from "../currency/ui/currency-form/currency-form.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-currency-dashboard",
@@ -16,32 +27,73 @@ import { MatCardModule } from "@angular/material/card";
   imports: [
     AsyncPipe,
     MatGridListModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
     MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    CurrencyFormComponent,
+    CurrencyResultComponent,
+    CurrencyLineChartComponent,
   ],
+  providers: [CurrencyFormService],
 })
 export class CurrencyDashboardComponent {
-  card2 = { cols: 1, rows: 1 };
-  card3 = { cols: 1, rows: 2 };
-  card4 = { cols: 1, rows: 1 };
+  #currencyHttpService = inject(CurrencyHttpService);
+  #currencyStore = inject(CurrencyStore);
+  #breakpointObserver = inject(BreakpointObserver);
 
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Medium])
-      .subscribe(({ matches }) => {
-        if (matches) {
-          // Adjust layout for small screens
-          this.card2 = { cols: 2, rows: 1 };
-          this.card3 = { cols: 2, rows: 1 };
-          this.card4 = { cols: 2, rows: 1 };
-        } else {
-          // Adjust layout for larger screens
-          this.card2 = { cols: 1, rows: 1 };
-          this.card3 = { cols: 1, rows: 2 };
-          this.card4 = { cols: 1, rows: 1 };
-        }
-      });
-  }
+  currencyListResource = this.#currencyHttpService.getCurrencyListResource();
+
+  convert = this.#currencyStore.convert;
+
+  amount = this.#currencyStore.amount;
+
+  selectedSymbol = this.#currencyStore.selectedSymbol;
+
+  ratesResource = this.#currencyStore.ratesResource;
+
+  chartRatesResource = this.#currencyStore.chartResource;
+
+  to = computed(() => {
+    const data = this.convert();
+    const { to } = data;
+    return to;
+  });
+
+  isMatch$ = this.#breakpointObserver
+    .observe([Breakpoints.Handset, Breakpoints.Small])
+    .pipe(
+      map((result) => result.matches),
+      shareReplay(1)
+    );
+
+  card1$: Observable<{ cols: number; rows: number }> = this.isMatch$.pipe(
+    map((isMatch) => {
+      if (isMatch) {
+        return { cols: 2, rows: 1 };
+      } else {
+        return { cols: 1, rows: 1 };
+      }
+    })
+  );
+  card2$: Observable<{ cols: number; rows: number }> = this.isMatch$.pipe(
+    map((isMatch) => {
+      if (isMatch) {
+        return { cols: 2, rows: 1 };
+      } else {
+        return { cols: 1, rows: 2 };
+      }
+    })
+  );
+  card3$: Observable<{ cols: number; rows: number }> = this.isMatch$.pipe(
+    map((isMatch) => {
+      if (isMatch) {
+        return { cols: 2, rows: 1 };
+      } else {
+        return { cols: 1, rows: 1 };
+      }
+    })
+  );
 }
